0888871c2295cb447e3f4039eab30865e0872723
YanaLeon <yana.leonovich2019@gmail.com>
2022-11-30 21:18:10 +0300

    fix: change method

--- VOWELS/script.js
+++ VOWELS/script.js
@@ -5,7 +5,7 @@ function countVowels (string){
     let vowels = ['а', 'о', 'е', 'и', 'у', 'ю', 'я', 'ы', 'э', 'ё'];
     let count = 0;
     for(let i = 0; i < array.length; i++){
-        if(vowels.indexOf(array[i])){
+        if(vowels.indexOf(array[i]) != -1){
             count++;
         }
     }
