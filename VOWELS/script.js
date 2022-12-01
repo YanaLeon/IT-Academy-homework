49e08f4947e0bea93d7707d285e01915120f5314
YanaLeon <yana.leonovich2019@gmail.com>
2022-12-01 19:05:50 +0300

    fix: change argument function

--- VOWELS/script.js
+++ VOWELS/script.js
@@ -1,7 +1,7 @@
 let string = prompt('Введите ваш текст');
 
-function countVowels (string){
-    let array = string.toLowerCase().split('');
+function countVowels (str){
+    let array = str.toLowerCase().split('');
     let vowels = ['а', 'о', 'е', 'и', 'у', 'ю', 'я', 'ы', 'э', 'ё'];
     let count = 0;
     for(let i = 0; i < array.length; i++){
