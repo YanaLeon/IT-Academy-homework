d38cc6e92c2c5ba176059170bb0c102be0fb04f6
YanaLeon <yana.leonovich2019@gmail.com>
2022-12-08 01:10:52 +0300

    fix: function

--- ANKETA/script2.js
+++ ANKETA/script2.js
@@ -3,13 +3,20 @@ let phrase = prompt ("Напишите здесь свой текст")
 function redactorForPhraseNew (str){
     let countRight = 0;
     let countLeft = 0;
+    let result = true;
     for (let i = 0; i < str.length; i++){
         if(str[i] === ' '){
             countRight += 1;
         } else {
+            result = false;
             break;
         }
     }
+    if(result === true){
+        console.log(result);
+        return str = 'Здесь только пробелы';
+    }
+
     for (let i = str.length - 1; i >= 0; i--){
         if(str[i] === ' '){
             countLeft += 1;
@@ -17,9 +24,15 @@ function redactorForPhraseNew (str){
             break;
         }
     }
-    str = str.slice(countRight, str.length - countLeft);
-    console.log(str.length);
-    return `*${str}*`;
+
+    if(countRight === 0 && countLeft === 0){
+        console.log(countRight, countLeft, 'нет пробелов');
+        return str;
+    } else {
+        str = str.slice(countRight, str.length - countLeft);
+        console.log(str.length);
+        return str;
+    }
 }
 
-let phraseResult = alert(redactorForPhraseNew (phrase));
+let phraseResult = alert(`*${redactorForPhraseNew (phrase)}*`);
--- MOOD/script2.js
+++ MOOD/script2.js
@@ -1,51 +1,27 @@
 let phrase = prompt("Введите текст для проверки");
 
 function isPalindrom (str){
-    let result = true;
+    let count = 0;
     str = str.toLowerCase().replace(/\s|[!,."'?]/g, '');
-    let strCopy = str;
     for (let i = 0; i < str.length; i++){
-        for (let j = strCopy.length - 1; j >= 0; j--){
-            if(str[i] === strCopy[j]){
-                strCopy = strCopy.slice(0, -1)
-                break;
-            } else {
-                result = false;
+        if(str[i] === str[str.length - i - 1]){
+            count++;
+            if(count >= 2){
+               return true;
             }
+        } else {
+            return false;
         }
     }
-    return result;
 }
-
 console.log(isPalindrom (phrase))
+
 let result = () => {
     if (isPalindrom (phrase)){
-        return alert ('Это палиндром!');
-    } else {
-        return alert ('Увы, это не палиндром!');
-    }
-}
-result()
-//===========================================
-let phrase2 = prompt("Введите текст для проверки");
-
-function isPalindrom2 (str){
-    let str2 = '';
-    str = str.toLowerCase().replace(/\s|[!,."'?]/g, '');
-    for(let i = str.length - 1; i >=0; i--){
-        str2 += str[i];
-    }
-    return str === str2;
-}
-
-console.log(isPalindrom2 (phrase2))
-
-let result2 = () => {
-    if (isPalindrom2 (phrase2)){
         return alert('Это палиндром!');
     } else {
         return alert('Увы, это не палиндром!');
     }
 }
-result2()
+result()
 
