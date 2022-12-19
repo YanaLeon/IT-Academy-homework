746084aa536932611711f7f09d37e5f97fc44201
YanaLeon <yana.leonovich2019@gmail.com>
2022-12-19 20:06:14 +0300

    init: task roots and drink_hash_func

---
+++ DRINKS_HASH_FUNC/script.js
@@ -0,0 +1,29 @@
+function HashStorageFunc (drink, alcohol, recipe) {
+    let self = this;
+
+    self[drink] = {'алкогольный': alcohol, 'рецепт' : recipe};
+
+    self.addValue = function (drink, alcohol, recipe){
+        self[drink] = {'алкогольный': alcohol, 'рецепт' : recipe};
+    }
+    self.getValue = function (drink){
+        return this[drink];
+    }
+    self.deleteValue = function (drink){
+        if(drink in self){
+            delete self[drink];
+            return true;
+        } else {
+            return false;
+        }
+    }
+    self.getKeys = function (){
+        let array = [];
+        for (let key in self){
+            if(typeof self[key] != 'function'){
+                array.push(key);
+            }
+        }
+        return array;
+    }
+}
