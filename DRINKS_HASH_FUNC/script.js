5d1148a249d67d4fccafea02d49df502c8ac398c
YanaLeon <yana.leonovich2019@gmail.com>
2022-12-20 20:44:12 +0300

    fix: change function

--- DRINKS_HASH_FUNC/script.js
+++ DRINKS_HASH_FUNC/script.js
@@ -1,17 +1,15 @@
 function HashStorageFunc () {
-    let storage = 'storage';
-    this[storage] = {};
+    this.storage = {};
 
-    this.addValue = function (key, [...value]){
-        this[storage][key] = value;
-        return this[storage];
+    this.addValue = function (key, value){
+        this.storage[key] = value;
     }
     this.getValue = function (key){
-        return this[storage][key];
+        return this.storage[key];
     }
     this.deleteValue = function (key){
-        if(key in this[storage]){
-            delete this[storage][key];
+        if(key in this.storage){
+            delete this.storage[key];
             return true;
         } else {
             return false;
@@ -19,9 +17,19 @@ function HashStorageFunc () {
     }
     this.getKeys = function (){
         let array = [];
-        for (let key in this[storage]){
+        for (let key in this.storage){
             array.push(key);
         }
         return array;
     }
 }
+let obj = new HashStorageFunc();
+console.log(obj);
+console.log(obj.addValue('Маргарита', ['нет', 'простой']));
+console.log(obj.addValue('Обычный', ['да', 'сложный']));
+console.log(obj);
+console.log(obj.getValue('Обычный'));
+console.log(obj.deleteValue('Обычный'));
+console.log(obj.deleteValue('sdrfg'));
+console.log(obj.addValue('Обычный', ['да', 'сложный']));
+console.log(obj.getKeys());
